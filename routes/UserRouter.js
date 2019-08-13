864deefab928b0be37bb124ee80d217003c1f07c
monstermania <zeeblee21@gmail.com>
2019-08-13 10:44:04 -0700

    successfully completed user crud routes and seperated into routes and controllers

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -3,8 +3,17 @@ const
     UserRouter = express.Router(),
     User = require('../controllers/user');
 
+// CREATE
 UserRouter.post('/create', User.create);
 // READ ALL (Index)
 UserRouter.get('/', User.index);
+// READ ONE
+UserRouter.get('/:id', User.show);
+//UPDATE
+UserRouter.patch('/:id', User.update);
+//DELETE
+UserRouter.delete('/:id', User.destroy);
+
+
 
 module.exports = UserRouter;
